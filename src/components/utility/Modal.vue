a6dcec812d34f74c14b892d7bc4ec0687cf6bf3e
Debadeep Sen <dsen@msi.home>
2024-01-10 22:23:59 +0530

    Close button tweaks

--- src/components/utility/Modal.vue
+++ src/components/utility/Modal.vue
@@ -9,8 +9,9 @@
       v-on:click.stop=""
     >
       <button
-        class="absolute top-0 right-[6px] text-2xl"
+        class="absolute top-[6px] right-[6px] text-3xl w-8 h-8 bg-zinc-200 hover:bg-zinc-300 rounded-full flex justify-center items-center pb-2"
         v-on:click="() => $emit('close')"
+        title="Close this dialog"
       >
         &times;
       </button>
