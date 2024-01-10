0d96409e09f2b45feac35680f077df6a6099da67
Debadeep Sen <dsen@msi.home>
2024-01-10 21:41:16 +0530

    Click outside

--- src/components/utility/Modal.vue
+++ src/components/utility/Modal.vue
@@ -2,8 +2,12 @@
   <div
     v-show="show"
     class="bg-zinc-600/70 fixed left-0 top-0 w-screen h-screen z-[100] flex justify-center items-center"
+    v-on:click="() => $emit('close')"
   >
-    <div class="rounded-lg shadow-xl p-4 bg-white w-full lg:min-w-[300px] max-w-[600px] relative">
+    <div
+      class="rounded-lg shadow-xl p-4 bg-white w-full lg:min-w-[300px] max-w-[600px] relative"
+      v-on:click.stop=""
+    >
       <button
         class="absolute top-0 right-[6px] text-2xl"
         v-on:click="() => $emit('close')"
